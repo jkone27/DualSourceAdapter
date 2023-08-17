318fbc7565e9083a494efc2aa1ab3887ad2978a5
jkone27 <jkone27@users.noreply.github.com>
2023-08-17 15:37:58 +0000

    first commit

---
+++ src/DualSourceAdapter/Library.fs
@@ -0,0 +1,70 @@
+﻿namespace DualSourceAdapter
+
+open System
+open System.Threading.Tasks
+open System.Collections.Generic
+open System.Threading
+
+module Adapter =
+
+    type MigrationSource = 
+        | Legacy = 0 
+        | New = 1
+
+    type MigrationOption = {
+            Sources: MigrationSource []
+            Active: MigrationSource
+            IsCompare: bool
+        }
+        with member this.IsSingleSource migrationSource = 
+                this.Sources 
+                |> Seq.tryExactlyOne
+                |> Option.map (fun o -> o = migrationSource)
+                |> Option.defaultValue false
+        
+
+    let secondaryTask lazySecondaryTask runningPrimaryTask compareFunction  = 
+        backgroundTask {
+            printfn "BACKGROUND TASK START"
+            try
+                let! primaryResult = runningPrimaryTask
+                let! secondaryResult = lazySecondaryTask()
+                compareFunction primaryResult secondaryResult
+                ()
+                printfn "BACKGROUND TASK STOP"
+            with e ->
+                printfn "BACKGROUND SECONDARY EX: %A" e.Message
+                ()
+        }
+
+    let migrate 
+        (opt : MigrationOption) 
+        (input: 'a) 
+        (legacyFunc: 'a -> 'b Task) 
+        (newFunc: 'a -> 'b Task) 
+        (compareFunc: 'b -> 'b -> unit) 
+            : 'b Task = 
+        task {
+
+            if opt.Sources |> Seq.isEmpty then
+                failwith "no sources specified"
+
+            if opt.IsSingleSource MigrationSource.Legacy then
+                return! legacyFunc(input) 
+            else if opt.IsSingleSource MigrationSource.New then
+                return! newFunc(input)
+            else
+                let (active,secondary) = 
+                    if opt.Active = MigrationSource.Legacy then
+                        legacyFunc, newFunc
+                    else
+                        newFunc, legacyFunc
+                
+                if opt.IsCompare then
+                    let activeResultTask = active(input)
+                    let lazySecondary = fun () -> secondary(input)
+                    let! _ = secondaryTask lazySecondary activeResultTask compareFunc
+                    return! activeResultTask
+                else
+                    return! active(input)
+        }
---
+++ test/DualSourceAdapter.Tests/Library.fs
@@ -0,0 +1,126 @@
+﻿namespace DualSourceAdapter.Tests
+
+open Xunit
+open System
+open System.Threading.Tasks
+open System.Collections.Generic
+open System.Threading
+open DualSourceAdapter.Adapter
+
+module Utils =
+    let rnd = new Random()
+
+module FakeContracts =
+
+    type CreateOrderRequest = { Test: string }
+    type CreateOrderResponse = { IdResult: int }
+
+    type GetOrderRequest = { Id: int }
+    type GetOrderResponse = { Content: string }
+
+module Legacy =
+
+    let legacyDB = new Dictionary<int, string>()
+
+    open FakeContracts
+    let writeOrder createOrderRequest = task {
+        printfn $"OLD WRITE: {createOrderRequest.Test}"
+        let id = Utils.rnd.Next()
+        legacyDB.Add(id, createOrderRequest.Test)
+        return { IdResult = id }
+    }
+
+    let getOrder getOrderRequest = task {
+        printfn $"OLD READ: {getOrderRequest.Id}"
+        return { Content = legacyDB[getOrderRequest.Id] }
+    }
+
+module New =
+    open FakeContracts
+
+    let newDB = new Dictionary<int, string>()
+
+    let writeOrder createOrderRequest = task {
+        printfn $"NEW WRITE: {createOrderRequest.Test}"
+        let id = Utils.rnd.Next()
+        newDB.Add(id, createOrderRequest.Test)
+        return { IdResult = id }
+    }
+
+    let getOrder getOrderRequest = task {
+        printfn $"NEW READ: {getOrderRequest.Id}"
+        return { Content = newDB[getOrderRequest.Id] }
+    }
+
+module TestMigration =
+
+    open FakeContracts
+
+    let compareFn a b =
+            printfn "COMPARE!!!!"
+    let writeMigrate (option: MigrationOption) input = 
+        migrate option input Legacy.writeOrder New.writeOrder compareFn
+
+    let readMigrate (option: MigrationOption) input = 
+        migrate option input Legacy.getOrder New.getOrder compareFn
+
+
+    let migration option =
+        task {
+
+            let createOrder = { Test = "test" }
+
+            let! writeResult = writeMigrate option createOrder
+
+            let! readResult = readMigrate option { Id = writeResult.IdResult }
+
+            return readResult
+
+        }
+
+
+module Options =
+
+    let LegacyActiveDual = { 
+        Sources = [| MigrationSource.Legacy ; MigrationSource.New |]
+        Active = MigrationSource.Legacy
+        IsCompare = true
+    }
+
+    let NewActiveDual = { 
+        Sources = [| MigrationSource.Legacy ; MigrationSource.New |]
+        Active = MigrationSource.New
+        IsCompare = true
+    }
+
+    let Legacy = { 
+        Sources = [| MigrationSource.Legacy |]
+        Active = MigrationSource.New
+        IsCompare = false
+    }
+
+    let New = { 
+        Sources = [| MigrationSource.New |]
+        Active = MigrationSource.New
+        IsCompare = false
+    }
+
+module Tests =
+
+    [<Fact>]
+    let ``test sources picker``() =
+            
+        Options.Legacy.IsSingleSource MigrationSource.Legacy |> Assert.True
+        Options.New.IsSingleSource MigrationSource.New |> Assert.True
+        Options.LegacyActiveDual.IsSingleSource MigrationSource.New |> Assert.False
+        Options.NewActiveDual.IsSingleSource MigrationSource.New |> Assert.False
+
+    [<Fact>]
+    let ``test migration adapter when legacy active and dual, ok``() =
+        task {
+
+            let! res = TestMigration.migration Options.LegacyActiveDual
+
+            Assert.NotEmpty(res.Content)
+
+        }
